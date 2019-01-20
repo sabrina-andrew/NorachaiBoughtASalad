5b4ef99e63f485d4f29620b903a3dc891349108f
Sabrina Andrew <sabrina.rae.andrew@gmail.com>
2019-01-20 15:12:41 -0500

    First Commit

---
+++ NorachaiBoughtASalad/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace NorachaiBoughtASalad
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //After paying \$5.12 for a salad, Norachai has 27.10. How much money did he have before buying the salad?
+
+            decimal salad = 5.12M;
+            decimal has = 27.10M;
+            decimal had = has - salad;
+            Console.WriteLine("Before buying the salad, Norachai had $" + had + " in his wallet.");
+        }
+    }
+}
